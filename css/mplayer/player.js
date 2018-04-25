afaf52fb80be569d5de2c4afeb182aa327da73ae
NiceriPro <xzf411235@163.com>
2018-04-25 14:22:08 +0800

    update

--- css/mplayer/player.js
+++ css/mplayer/player.js
@@ -347,7 +347,7 @@ function showNotification(info) {
 		$('.qplayer-notification').css({"width":width,"transition":"all .7s ease"});
 		$('.qplayer-notification .close').delay(500).show(0);
 	},1500);
-	autoClearTimer = setTimeout("if ($('.qplayer-notification').length>0) {closeNotification()}",8000);
+	autoClearTimer = setTimeout("if ($('.qplayer-notification').length>0) {closeNotification()}",3000);
 }
 
 
